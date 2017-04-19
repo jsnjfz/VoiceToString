3c151060a9c92b48d4eb1badc05c564dae82c68b
fanzhou <jsnjfz@gmail.com>
2017-04-19 15:41:43 +0800

    调整超时时间和记录日志

--- src/service/Convert.java
+++ src/service/Convert.java
@@ -4,6 +4,7 @@ package service;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -30,7 +31,9 @@ public class Convert extends HttpServlet {
         PrintWriter out = res.getWriter();  
 		String appid = req.getParameter("appid");//"58ddb416";       //您的appid，如"5848d773"
 		String secret_key = req.getParameter("secretkey");//"02d739f406931fd499a5266ffa129b3a";  //您的secret_key，如"b849c87a8bc2c7ww68b6dfbddee6dc35"
+		//84bf7f5859fd40cd8c015b14217010fb
 		String filename = req.getParameter("filename");
+		System.out.println("*****XXX***" + new Date() + "*****");
 		if(filename==null||filename.equals("")){
 	        out.print("error!");  
 	        out.flush();
@@ -48,17 +51,22 @@ public class Convert extends HttpServlet {
 			}
 			Message message = null;
 			try {
+				System.out.println("*****000***" + new Date() + "*****");
 				String savePath = this.getServletContext().getRealPath("/WEB-INF/upload");
+				System.out.println("*****111***" + new Date() + "*****");
 				message = client.lfasr_upload(savePath+"/"+filename);
+				System.out.println("*****222***" + new Date() + "*****");
 			} catch (LfasrException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}  //需要转写的音频文件的路径，如"E:\\20160716am-as-shenzhen-Alex.mp3"
 			catch (Exception e1) {
+				System.out.println("*****Error*****");
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			} 
 			
+			System.out.println("*****3333*****");
 			if(message.getErr_no()==0 && message.getOk()==0){
 				taskId = message.getData();
 		        //将数据拼接成JSON格式  
